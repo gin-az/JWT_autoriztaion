5707332a85b8453a6411a300d23ecb116a59e247
AGindullin <GA>
2023-06-27 09:54:12 +0300

    full2

---
+++ client/src/App.tsx
@@ -0,0 +1,63 @@
+import React, { useContext, useEffect, useState } from "react";
+import LoginForm from "./components/LoginForm";
+import { Context } from "./index";
+import { observer } from "mobx-react-lite";
+import { IUser } from "./models/IUser";
+import UserService from "./services/UserService";
+
+function App() {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, []);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response?.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Загрузка. . .</div>;
+  }
+
+  if (!store.isAuth) {
+    return (
+      <>
+        <LoginForm />
+        <button onClick={getUsers}> Получить пользователей</button>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <h1>
+        {store.isAuth
+          ? `Пользователь авторизован ${store.user.email}`
+          : "АВТОРИЗУЙТЕСЬ"}
+      </h1>
+      <h1>
+        {store.user.isActivated
+          ? "Аккаунт подтвержден по почте"
+          : "Подтвердите аккаунт"}
+      </h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}> Получить пользователей</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
+    </div>
+  );
+}
+
+export default observer(App);
